5989c19d7ad925d7076b81c69d93b44f96d6ade8
denisabonda <denocheboy@gmail.com>
2023-03-18 13:38:50 +0300

    ready-to-deploy

--- client/src/pages/yourBooks/YourBooks.jsx
+++ client/src/pages/yourBooks/YourBooks.jsx
@@ -7,32 +7,37 @@ const YourBooks = () => {
     const user=JSON.parse(localStorage.getItem("user"))
     const {data,loading}=useQuery(GET_AUTHOR,{
         variables:{
-            id:user.id
+            id:user?.id
         }
     })
     return (
-        <div className="yourBooksWrapper">
-            <h4 className="yourBookHeader">Book Written by you</h4>
-            <span className="yourBookTotalCount">Total Count :{data?.author?.books.length}</span>
-            <div className="yourBookInnerWrapper">
-                <h3 className="categoriesHeader">Categories</h3>
-                {
-                    category.map(categ=>(
-                        <div className="categoryWrapper">
-                            <h5 className="categoryHeader">{categ}</h5>
-                            {data?.author?.books.filter(book=>book.genre===categ).map(book=>(
-                                <div className='singleBook'>
-                                  <span className="yourBookName">Book Name: {book.name}</span>
-                                  <p className="yourBookPrice">Book Price: {book.price}</p>
-                                  <p className="yourBookIsFeatured">isFeatured: {book.isFeatured?"true":"false"}</p>
+        
+            !data?<span>No Available Books Please upload 1</span>:(
+                <div className="yourBooksWrapper">
+                    <h4 className="yourBookHeader">Book Written by you</h4>
+                    <span className="yourBookTotalCount">Total Count :{data?.author?.books.length}</span>
+                    <div className="yourBookInnerWrapper">
+                        <h3 className="categoriesHeader">Categories</h3>
+                        {
+                            category.map(categ=>(
+                                <div className="categoryWrapper">
+                                    <h5 className="categoryHeader">{categ}</h5>
+                                    {data?.author?.books.filter(book=>book.genre===categ).map(book=>(
+                                        <div className='singleBook'>
+                                          <span className="yourBookName">Book Name: {book.name}</span>
+                                          <p className="yourBookPrice">Book Price: {book.price}</p>
+                                          <p className="yourBookIsFeatured">isFeatured: {book.isFeatured?"true":"false"}</p>
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
-                        </div>
-                    ))
-                }
-            </div>
+                            ))
+                        }
+                    </div>
+        
+                </div>
 
-        </div>
+            )
+        
     )
 }
 
