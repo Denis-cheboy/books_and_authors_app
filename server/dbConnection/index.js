2282fd59b7c5bae8dd08bd525154b584c541b0c4
denisabonda <denocheboy@gmail.com>
2023-03-18 13:23:39 +0300

    configured mongo atlas

--- server/dbConnection/index.js
+++ server/dbConnection/index.js
@@ -1,8 +1,9 @@
 const mongoose=require("mongoose")
+require("dotenv").config()
 
 const connectDB=async()=>{
     try{
-      await mongoose.connect("mongodb://127.0.0.1:27017/graphQLDB",{
+      await mongoose.connect(process.env.MONGO_DB_URI,{
         useNewUrlParser:false,
         useUnifiedTopology:false
       })
